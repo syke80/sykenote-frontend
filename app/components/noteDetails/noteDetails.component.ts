8a7fb9b0eec412bac64060027fd4561f43da68ee
Attila Keszthelyi <syke80@gmail.com>
2017-01-10 09:10:35 +0100

    UI: use editable div instead of textarea

--- app/components/noteDetails/noteDetails.component.ts
+++ app/components/noteDetails/noteDetails.component.ts
@@ -1,9 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Pipe, PipeTransform } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { NoteModel } from '../../models/note.model';
 import { GetNoteResponseModel } from '../../models/getNoteResponse.model';
 import { NoteService } from '../../services/note.service';
 import { AuthenticationService } from '../../services/authentication.service';
+
+@Pipe({name: 'textToHtml'})
+export class TextToHtml implements PipeTransform {
+    transform(content: string): string {
+        let updatedContent: string = content.replace(/(?:\r\n|\r|\n)/g, '<br />');
+
+        return updatedContent;
+    }
+}
 
 @Component({
     moduleId: module.id,
@@ -14,6 +23,8 @@ import { AuthenticationService } from '../../services/authentication.service';
 export class NoteDetailsComponent {
     @Input() id: number;
     note: NoteModel;
+    actualContent: string;
+    isDirty: boolean;
 
     constructor(
         private noteService: NoteService,
@@ -26,6 +37,8 @@ export class NoteDetailsComponent {
             this.noteService.getNote(this.id)
                 .then((response: GetNoteResponseModel) => {
                     this.note = response.note;
+                    this.actualContent = this.note.content;
+                    this.isDirty = false;
                 })
                 .catch(error => {
                     this.handleHttpError.call(this, error);
@@ -33,16 +46,36 @@ export class NoteDetailsComponent {
         }
     }
 
+    private updateModel(): void {
+        this.note.content = this.actualContent;
+    }
+
+    getContentHtml(): string {
+        if (this.note) {
+            return this.note.content.replace(/(?:\r\n|\r|\n)/g, '<br />');
+        }
+        else {
+            return '';
+        }
+    }
+
     onSubmitNoteDetailsForm(): void {
+        this.updateModel();
         this.noteService.update(this.note)
-            .then( () => true ) // TODO: show popup with msg
+            .then( () => {
+                this.isDirty = false;
+            } ) // TODO: show popup with msg
             .catch( error => {
                 this.handleHttpError.call(this, error);
             });
     }
 
-    onTextContentChange(): void {
-        alert('text content has been changed');
+    onKeyPressedInContent($event: KeyboardEvent): void {
+        let targetElement: HTMLElement;
+
+        targetElement = <HTMLElement> $event.target;
+        this.actualContent = targetElement.innerText;
+        this.isDirty = true;
     }
 
     ngOnChanges(): void {
